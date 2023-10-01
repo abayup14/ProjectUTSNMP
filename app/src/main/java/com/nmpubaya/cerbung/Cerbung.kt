136ca3784917e04883bb2b804a0350a15fc2da3a
abayup14 <abayup1498@gmail.com>
2023-10-01 09:34:53 +0700

    Create Cerbung.kt

---
+++ app/src/main/java/com/nmpubaya/cerbung/Cerbung.kt
@@ -0,0 +1,10 @@
+package com.nmpubaya.cerbung
+
+data class Cerbung(
+    val title: String,
+    val description: String,
+    val image_url: String,
+    val genre: String,
+    val access: String,
+    val paragraph: String
+)
