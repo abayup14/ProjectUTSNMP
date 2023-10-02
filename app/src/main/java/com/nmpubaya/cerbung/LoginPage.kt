dcbbe7cf49e611e9be7918a429bbac06a5ff7a87
abayup14 <abayup1498@gmail.com>
2023-10-02 16:48:18 +0700

    Update LoginPage, RegisterPage, Add Class User

--- app/src/main/java/com/nmpubaya/cerbung/LoginPage.kt
+++ app/src/main/java/com/nmpubaya/cerbung/LoginPage.kt
@@ -3,20 +3,40 @@ package com.nmpubaya.cerbung
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.nmpubaya.cerbung.databinding.ActivityLoginPageBinding
 
 class LoginPage : AppCompatActivity() {
     private lateinit var binding:ActivityLoginPageBinding
+
+    companion object {
+        val LIST_USER = arrayListOf(
+            User("Bayu0123", "halo", "1234")
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnLogin.setOnClickListener {
+            val username = binding.txtUsernameLogin.text.toString()
+            val password = binding.txtPasswordLogin.text.toString()
+
+            for (user in LIST_USER) {
+                if (username == user.username && password == user.password) {
+                    Toast.makeText(this, "Welcome to Cerbung, $username", Toast.LENGTH_SHORT).show()
+                    break
+                } else {
+                    Toast.makeText(this, "Login gagal. Silahkan coba lagi", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
         binding.btnCreateAccount.setOnClickListener{
             val intent = Intent(this, RegisterPage::class.java)
             startActivity(intent)
         }
-
     }
 }
--- app/src/main/java/com/nmpubaya/cerbung/RegisterPage.kt
+++ app/src/main/java/com/nmpubaya/cerbung/RegisterPage.kt
@@ -3,10 +3,16 @@ package com.nmpubaya.cerbung
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.nmpubaya.cerbung.databinding.ActivityRegisterPageBinding
 
 class RegisterPage : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterPageBinding
+
+    companion object {
+        val USER = "user"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterPageBinding.inflate(layoutInflater)
@@ -15,6 +21,24 @@ class RegisterPage : AppCompatActivity() {
         binding.btnCancel.setOnClickListener{
             intent = Intent(this, LoginPage::class.java)
             startActivity(intent)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            val username = binding.txtUsernameRegister.text.toString()
+            val profile_url = binding.txtUrlRegister.text.toString()
+            val password = binding.txtPasswordRegister.text.toString()
+
+            if (password == binding.txtRePasswordRegister.text.toString()) {
+                val user = User(username, profile_url, password)
+                LoginPage.LIST_USER.add(user)
+                Toast.makeText(this, "Berhasil. Silahkan login menggunakan akun anda", Toast.LENGTH_SHORT).show()
+                intent = Intent(this, LoginPage::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Password dengan Re-Password tidak sama. Silahkan ulangi", Toast.LENGTH_SHORT).show()
+                binding.txtPasswordRegister.text.clear()
+                binding.txtRePasswordRegister.text.clear()
+            }
         }
     }
 }
